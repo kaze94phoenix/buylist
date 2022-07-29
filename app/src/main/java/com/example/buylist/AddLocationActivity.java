25213213c1f800402f20ba6350ff4fcb54d28a0c
kaze94phoenix <phoenix.kazekage@gmail.com>
2022-07-29 20:48:22 +0200

    ...

---
+++ app/src/main/java/com/example/buylist/AddLocationActivity.java
@@ -0,0 +1,13 @@
+package com.example.buylist;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddLocationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_location);
+    }
+}
