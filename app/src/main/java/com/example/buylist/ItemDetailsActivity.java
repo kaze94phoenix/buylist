9a877094c0aee17a3d334e950839679f0b45b792
kaze94phoenix <phoenix.kazekage@gmail.com>
2022-08-18 12:58:40 +0200

    - Add Item Location and price function functional

--- app/src/main/java/com/example/buylist/ItemDetailsActivity.java
+++ app/src/main/java/com/example/buylist/ItemDetailsActivity.java
@@ -2,70 +2,139 @@ package com.example.buylist;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.buylist.adapters.ItemLocationAdapter;
 import com.example.buylist.adapters.ShoppingItemAdapter;
 import com.example.buylist.models.DataManager;
 import com.example.buylist.models.Item;
+import com.example.buylist.models.ItemLocation;
+import com.example.buylist.models.ItemType;
+import com.example.buylist.models.Location;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class ItemDetailsActivity extends AppCompatActivity {
-    private TextView itemName,itemDescription, itemPrice, itemType;
+    private TextView itemName, itemDescription, itemPrice, itemType;
     private DataManager dataManager;
     private ArrayList<Item> items;
+    private ArrayList<Location> locations;
     private Intent intent;
     private AlertDialog.Builder dialogBuilder;
     private AlertDialog dialog;
+    private int itemId;
+    private RecyclerView recyclerView;
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private ItemLocationAdapter itemLocationAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_details);
 
+        dataManager = new DataManager(this);
         intent = getIntent();
-        int itemId = intent.getIntExtra(ShoppingItemAdapter.EXTRA_ITEM_ID,0);
+        itemId = intent.getIntExtra(ShoppingItemAdapter.EXTRA_ITEM_ID, 0);
+
+        items = dataManager.getItems();
+        locations = dataManager.getLocations();
 
         itemName = findViewById(R.id.itemDName);
         itemDescription = findViewById(R.id.itemDDescription);
         itemPrice = findViewById(R.id.itemDPrice);
         itemType = findViewById(R.id.itemDType);
 
-        dataManager = new DataManager(this);
-        items = dataManager.getItems();
-
-
         itemName.setText(items.get(itemId).getName());
         itemDescription.setText(items.get(itemId).getDescription());
         itemType.setText(items.get(itemId).getItemType().getName());
+
+        itemLocationAdapter = new ItemLocationAdapter();
+
+        itemLocationAdapter.setItemLocations(dataManager.getItemLocations());
+        itemLocationAdapter.setActivity(this);
+
+
+        recyclerView = findViewById(R.id.locationPricesRV);
+
+        recyclerView.setAdapter(itemLocationAdapter);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        swipeRefreshLayout = findViewById(R.id.swipeRefresh);
+
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                itemLocationAdapter.setItemLocations(dataManager.getItemLocations());
+                recyclerView.setAdapter(itemLocationAdapter);
+                recyclerView.setLayoutManager(new LinearLayoutManager(ItemDetailsActivity.this));
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
 
 
 
     }
 
-    public void addItemLocation(View view){
+    public void addItemLocation(View view) {
 
         dialogBuilder = new AlertDialog.Builder(this);
-        final View addItemLocationView = getLayoutInflater().inflate(R.layout.add_item_location_popup,null);
+        final View addItemLocationView = getLayoutInflater().inflate(R.layout.add_item_location_popup, null);
+        ArrayList<String> locationsNames = new ArrayList<>();
 
-        Button cancelBtn = addItemLocationView.findViewById(R.id.btnCancelItemLocation);
-
-        dialogBuilder.setView(addItemLocationView);
-        dialog = dialogBuilder.create();
-        dialog.show();
-
-
-        cancelBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                dialog.dismiss();
+        Spinner locationsSpinner = addItemLocationView.findViewById(R.id.locationSpinner);
+        if (dataManager.getItemTypes() != null) {
+            for (Location a : locations) {
+                locationsNames.add(a.getName());
             }
-        });
 
+            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, locationsNames);
+            locationsSpinner.setAdapter(arrayAdapter);
+
+            EditText price = addItemLocationView.findViewById(R.id.itemPriceTxt);
+            Button cancelBtn = addItemLocationView.findViewById(R.id.btnCancelItemLocation);
+            Button saveBtn = addItemLocationView.findViewById(R.id.btnSaveItemLocation);
+            Button addLocation = addItemLocationView.findViewById(R.id.goAddLocation);
+
+            dialogBuilder.setView(addItemLocationView);
+            dialog = dialogBuilder.create();
+            dialog.show();
+
+
+
+
+
+            saveBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    dataManager.addItemLocation(new ItemLocation(locations.get(locationsSpinner.getSelectedItemPosition()),items.get(itemId),Double.parseDouble(price.getText().toString())));
+                    itemLocationAdapter.notifyItemInserted(dataManager.getItemLocations().size()-1);
+                    Toast.makeText(ItemDetailsActivity.this, "Item Location Added", Toast.LENGTH_SHORT).show();
+                    dialog.dismiss();
+                }
+            });
+
+
+            cancelBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    dialog.dismiss();
+                }
+            });
+
+        }
     }
 }
