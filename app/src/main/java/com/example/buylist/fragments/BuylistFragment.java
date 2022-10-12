6fa4ed4f4a0c4dc1da881ee05538d53cb17ae252
Atarimo <phoenix.kazekage@gmail.com>
2022-10-12 14:26:20 +0200

    Code improvements and comments: moving the buylist list to the statistics for further usage. on the buylist will shall have a standard list to be changed or saved

--- app/src/main/java/com/example/buylist/fragments/BuylistFragment.java
+++ app/src/main/java/com/example/buylist/fragments/BuylistFragment.java
@@ -3,6 +3,7 @@ package com.example.buylist.fragments;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -15,17 +16,27 @@ import android.widget.TextView;
 import com.example.buylist.AddBuyListActivity;
 import com.example.buylist.BuyListListActivity;
 import com.example.buylist.R;
+import com.example.buylist.adapters.BuyListAdapter;
 import com.example.buylist.adapters.BuyListListAdapter;
 import com.example.buylist.models.DataManager;
+import com.example.buylist.models.Purchase;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link BuylistFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class BuylistFragment extends Fragment {
 
+    private AlertDialog dialog;
+    private AlertDialog.Builder dialogBuilder;
+    private RecyclerView buylist;
+    private DataManager dataManager;
+    private BuyListAdapter buyListAdapter;
+    private ArrayList<Purchase> purchases;
+    Intent intent;
 
     public BuylistFragment() {
         // Required empty public constructor
@@ -35,8 +46,24 @@ public class BuylistFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_buylist, container, false);
+
+        dataManager = new DataManager(getActivity());
+        buyListAdapter = new BuyListAdapter();
+
+
+
+        purchases = dataManager.getBuyLists().get(0).getPurchases();
+
+        buyListAdapter.setBuylist(purchases);
+
+        buylist = view.findViewById(R.id.buylist);
+
+        buylist.setAdapter(buyListAdapter);
+        buylist.setLayoutManager(new LinearLayoutManager(getContext()));
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_buylist, container, false);
+        return view;
     }
 
 
